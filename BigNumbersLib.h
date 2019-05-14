9e6e4fffe531ca2a6e23f6f83c695f171e570752
mllim15 <michelle.lim15@gmail.com>
2019-05-14 08:34:24 -0700

    Create BigNumbersLib.h

---
+++ BigNumbersLib.h
@@ -0,0 +1,20 @@
+/**
+ * @file BigNumbersLib.h
+ *
+ * @author Michelle Lim
+ *
+ * @version 1
+ *
+ * @brief This library was created for CS2560 Final Part 2, and will
+ * expand primitive data types and allow users to store big numbers that can
+ * store up to 1,00 digits long. In order to do this, a BigNumbers class
+ * will be created using TDD, that will use vectors from STL to store the digits.
+ * BigNumbers should have the same functionality as other numerical data types.
+ 
+ */
+
+#ifndef BigNumbersLib_h
+#define BigNumbersLib_h
+
+
+#endif /* BigNumbersLib_h */
